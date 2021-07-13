77acf427f9c1a82ad86b0ff74571c233916c527a
TheD4nM4n <thed4nm4n@gmail.com>
2021-07-13 10:59:02 -0400

    Added license disclaimer

--- src/main/java/com/thed4nm4n/enhancedregions/EnhancedRegions.java
+++ src/main/java/com/thed4nm4n/enhancedregions/EnhancedRegions.java
@@ -1,3 +1,17 @@
+/*
+Copyright 2021 Daniel Poe
+
+This file is a part of EnhancedRegions. EnhancedRegions is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+EnhancedRegions is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with EnhancedRegions. If not, see
+https://www.gnu.org/licenses/.
+ */
+
 package com.thed4nm4n.enhancedregions;
 
 // All required imports. Check pom.xml for repositories necessary to build this plugin yourself.
